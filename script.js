813305df9694ba4451c4b96ca1abf38895db2504
arif <arifedogawa97@gmail.com>
2024-07-17 05:37:31 +0700

    First version of Login Page

---
+++ script.js
@@ -0,0 +1,25 @@
+const UsernameField = document.getElementById("username");
+const PasswordField = document.getElementById("password");
+const LoginButton = document.getElementById("login-btn");
+const SignUpButton = document.getElementById("sign-up-btn");
+const ViewPassword = document.querySelector(".material-symbols-outlined");
+
+ViewPassword.addEventListener("click", function(){
+    if(PasswordField.type === "password"){
+        PasswordField.type = "text";
+        ViewPassword.innerText = "visibility_off";
+    }
+    else{
+        PasswordField.type = "password";
+        ViewPassword.innerText = "visibility";
+    }
+});
+
+LoginButton.addEventListener("click", function(){
+    alert("Berhasil login");
+});
+
+SignUpButton.addEventListener("click", function(){
+    alert("Maap ya belum redi");
+});
+
