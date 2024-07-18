9eeb35759a6247d245261ab16d751d564bd8b911
arif <arifedogawa97@gmail.com>
2024-07-18 12:15:54 +0700

    Add Password Requirement

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const PasswordField = document.getElementById("password");
 const LoginButton = document.getElementById("login-btn");
 const SignUpButton = document.getElementById("sign-up-btn");
 const ViewPassword = document.querySelector(".material-symbols-outlined");
+const PasswordReq = document.getElementById("password-requirement");
+
+PasswordField.addEventListener("focusin", function(){
+    PasswordReq.innerText = "*Password length min 8 character";
+    PasswordReq.style.marginTop = "5px";
+});
+PasswordField.addEventListener("focusout", function(){
+    PasswordReq.innerText = "";
+    PasswordReq.style.marginTop = "none";
+});
 
 ViewPassword.addEventListener("click", function(){
     if(PasswordField.type === "password"){
