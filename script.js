12f4138c96d39ba38bd084b1b646b0b6611c2a25
arif <arifedogawa97@gmail.com>
2024-07-20 12:30:01 +0700

    Add Signup Page

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-const UsernameField = document.getElementById("username");
-const PasswordField = document.getElementById("password");
+const LoginForm = document.querySelector(".login-form");
+const SignupForm = document.querySelector(".signup-form");
 const LoginButton = document.getElementById("login-btn");
-const SignUpButton = document.getElementById("sign-up-btn");
-const ViewPassword = document.querySelector(".material-symbols-outlined");
-const PasswordReq = document.getElementById("password-requirement");
+const PageSwitch = document.getElementById("page-switch");
+const PageStatus = document.querySelector(".page-status");
+const PasswordFields = document.querySelectorAll("#password-login, #password-signup");
+const ViewPasswords = document.querySelectorAll(".material-symbols-outlined");
+// const PasswordFields = document.querySelectorAll(".password");
+// const ViewPasswords = document.querySelectorAll(".material-symbols-outlined");
 
-ViewPassword.addEventListener("click", function(){
-    if(PasswordField.type === "password"){
-        PasswordField.type = "text";
-        ViewPassword.innerText = "visibility_off";
-    }
-    else{
-        PasswordField.type = "password";
-        ViewPassword.innerText = "visibility";
-    }
+ViewPasswords.forEach((ViewPassword, index) => {
+    ViewPassword.addEventListener("click", function(){
+        const PasswordField = PasswordFields[index];
+        if(PasswordField.type === "password"){
+            PasswordField.type = "text";
+            ViewPassword.innerText = "visibility_off";
+        } else {
+            PasswordField.type = "password";
+            ViewPassword.innerText = "visibility";
+        }
+    });
 });
 
 LoginButton.addEventListener("click", function(){
     alert("Berhasil login");
 });
 
-SignUpButton.addEventListener("click", function(){
-    alert("Maap ya belum redi");
+PageSwitch.addEventListener("click", function(){
+    if(LoginForm.style.display === "flex"){
+        LoginForm.style.display = "none";
+        SignupForm.style.display = "flex";
+        PageStatus.innerText = "Already have an account?"
+        PageSwitch.innerText = "Login";
+    }
+    else{
+        LoginForm.style.display = "flex";
+        SignupForm.style.display = "none";
+        PageStatus.innerText = "Doesn't have an account yet?"
+        PageSwitch.innerText = "Signup";
+    }
 });
 
